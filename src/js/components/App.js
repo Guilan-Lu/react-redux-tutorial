9dbea3dc1551a9faaa2a6229aaefd33686720408
email <dan.devguru@gmail.com>
2020-03-07 19:00:46 +0800

    asynchronous actions is added

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -8,8 +8,14 @@ const App = () => (
       <h2>Articles</h2>
       <List />
     </div>
-    <h2>Add a new article</h2>
-    <Form />
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
+    </div>
   </>
 );
 
